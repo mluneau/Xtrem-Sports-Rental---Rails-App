dc1750b331f54c09005a81d12c73749d1f4fdea0
Pierre Olive <pierre.olive4@gmail.com>
2021-02-16 16:05:01 +0100

    added select list of sports

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  SPORT_CATEGORIES = ['Ski', 'Snowboard', 'Kite Surf', 'Snowkite', 'Wingsuit', 'Highline']
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
