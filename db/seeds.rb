51fe5f6c5fde18171e508f857da8223137773e04
Nicolas Gauguez <76942939+NicolasGauguez@users.noreply.github.com>
2021-02-16 17:41:11 +0100

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Equipment.destroy_all
+User.destroy_all
+
+
+
+nico = User.new(first_name: "Nicolas", last_name: "Gauguez", email:"nico@xtrem.com", password:"password")
+nico.save!
+
+puts 'Creating equipments...'
+salomon_freeride = Equipment.new(title: "Ski Salomon freeride 2m", description: "Enjoy the puff this weekend with my skis. Brand new, 2021. Level: expert", address: "Serre Chevalier", daily_price: 25, user: nico, sport_category: 'Ski')
+salomon_freeride.save!
+
+
+atomic_freeride = Equipment.new(title: "Ski atomic_freeride", description: "Enjoy the puff this weekend with my skis. Brand new, 2021. Level: expert", address: "Serre Chevalier", daily_price: 25, user: nico, sport_category: 'Ski')
+atomic_freeride.save!
+
+salomon_freestyle = Equipment.new(title: "Ski Salomon freeride 2m", description: "Enjoy the puff this weekend with my skis. Brand new, 2021. Level: expert", address: "Serre Chevalier", daily_price: 25, user: nico, sport_category: 'Ski')
+salomon_freestyle.save!
+
+atomic_freestyle = Equipment.new(title: "Ski Salomon freeride 2m", description: "Enjoy the puff this weekend with my skis. Brand new, 2021. Level: expert", address: "Serre Chevalier", daily_price: 25, user: nico, sport_category: 'Ski')
+atomic_freestyle.save!
+
+black_crows = Equipment.new(title: "Ski Salomon freeride 2m", description: "Enjoy the puff this weekend with my skis. Brand new, 2021. Level: expert", address: "Serre Chevalier", daily_price: 25, user: nico, sport_category: 'Ski')
+black_crows.save!
+
+rossignol = Equipment.new(title: "Ski Salomon freeride 2m", description: "Enjoy the puff this weekend with my skis. Brand new, 2021. Level: expert", address: "Serre Chevalier", daily_price: 25, user: nico, sport_category: "Ski")
+rossignol.save!
