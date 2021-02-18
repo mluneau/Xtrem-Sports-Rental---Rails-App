f03823a716774d279c0433b7e3ab9cd2764cb1fb
Maxence Luneau <maxenceluneau@gmail.com>
2021-02-18 11:35:51 +0100

    added dashboard page, route and stylesheets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :categories]
+  
   def home
   end
+
+  def dashboard
+  end
+
 
   def categories
     @categories = ApplicationController::SPORT_CATEGORIES
